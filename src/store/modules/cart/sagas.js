be805e46885c41eba237422d16eb8556ea861b9b
Kilson <kilsonrs@gmail.com>
2019-10-04 15:52:36 -0300

    Separando actions

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,12 +1,35 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, put, select, all, takeLatest } from 'redux-saga/effects';
 
 import api from '../../../services/api';
-
-import { addToCartSuccess } from './actions';
+import { formatPrice } from '../../../util/format';
+import { addToCartSuccess, updateAmount } from './actions';
 
 function* addToCart({ id }) {
   const response = yield call(api.get, `/products/${id}`);
-  yield put(addToCartSuccess(response.data));
+  const productExists = yield select(state =>
+    state.cart.find(p => p.id === id)
+  );
+
+  if (productExists) {
+    const amount = productExists.amount + 1;
+    yield put(updateAmount(id, amount));
+  } else {
+    const data = {
+      ...response.data,
+      amount: 1,
+      priceFormatted: formatPrice(response.data.price),
+    };
+    yield put(addToCartSuccess(data));
+  }
+  // const productIndex = draft.findIndex(p => p.id === action.product.id);
+  // if (productIndex >= 0) {
+  //   draft[productIndex].amount += 1;
+  // } else {
+  //   draft.push({
+  //     ...action.product,
+  //     amount: 1,
+  //   });
+  // }
 }
 
 export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
